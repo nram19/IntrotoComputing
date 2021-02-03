44d5b1c693f35933a8b392eeae0813780c9b8010
nram19 <77447645+nram19@users.noreply.github.com>
2021-02-03 22:23:58 +0000

    Add files via upload

---
+++ HW 2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+colors = ['red', 'purple', 'yellow', 'black', 'white']
+for n in colors:
+    print(n)
+
+
+# In[2]:
+
+
+for n in range(10):
+    print(n)
+
+
+# In[3]:
+
+
+n = 0
+while n < 10:
+    print(n)
+    n += 1
+
+
+# In[4]:
+
+
+n = 0
+while n <=10:
+    print(n)
+    n = n + 1
+print("greater than 10")
+
+
+# In[6]:
+
+
+n = 1
+sum = 0
+while n <=30:
+    sum = sum + n
+    n = n + 1
+print(sum)
+
