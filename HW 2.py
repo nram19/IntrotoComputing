56bd4728093d45c7c3190a39943b108ed5bfe51f
nram19 <77447645+nram19@users.noreply.github.com>
2021-02-03 22:25:53 +0000

    Update HW 2.py

--- HW 2.py
+++ HW 2.py
@@ -1,41 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 colors = ['red', 'purple', 'yellow', 'black', 'white']
 for n in colors:
     print(n)
 
-
-# In[2]:
-
-
+    
 for n in range(10):
     print(n)
 
-
-# In[3]:
-
-
+    
 n = 0
 while n < 10:
     print(n)
     n += 1
 
-
-# In[4]:
-
-
+    
 n = 0
 while n <=10:
     print(n)
     n = n + 1
 print("greater than 10")
-
-
-# In[6]:
 
 
 n = 1
@@ -44,4 +26,3 @@ while n <=30:
     sum = sum + n
     n = n + 1
 print(sum)
-
