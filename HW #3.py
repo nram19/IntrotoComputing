4177577f59fe313630af26d1329a7da8e030c053
nram19 <77447645+nram19@users.noreply.github.com>
2021-02-16 14:47:53 +0000

    Update HW #3.py

--- HW #3.py
+++ HW #3.py
@@ -1,30 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 marks = {'Andy':88, 'Amy':  66, 'James':90, 'Jules': 55, 'Arthur':77}
 marks
-
-
-# In[2]:
 
 
 for k, v in marks.items():
     print('Name=', k, ', Grade=', v)
 
-
-# In[3]:
-
-
+    
 import statistics as stats
 print("Mean grade:" , stats.mean(marks.values()))
 print("Maximal grade:" , max(marks.values()))
 print("Minimal grade:", min(marks.values()))
-
-
-# In[4]:
 
 
 for k in marks:
@@ -33,16 +18,10 @@ for k in marks:
     print(k)
 
 
-# In[5]:
-
-
 for k in marks:
     if 'J' in k:
         continue
     print(k)
-
-
-# In[6]:
 
 
 for k, v in marks.items():
@@ -55,10 +34,6 @@ for k, v in marks.items():
 print(grade('Jules'))
 print(grade('Annie'))
 print(grade('James'))
-
-
-# In[ ]:
-
 
 
 
