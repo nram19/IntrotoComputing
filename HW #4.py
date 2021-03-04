8ac51c805d9727e35cc95f373e5e5fa296287208
nram19 <77447645+nram19@users.noreply.github.com>
2021-03-04 14:26:42 +0000

    Add files via upload

---
+++ HW #4.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+numbers = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+
+# In[3]:
+
+
+print(numbers)
+
+
+# In[23]:
+
+
+xint= np.arange(1, 7)
+yint= np.arange(5, 11)
+print(xint)
+print(yint)
+
+
+# In[24]:
+
+
+import matplotlib
+print(matplotlib.__version__)
+
+
+# In[25]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[26]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot(xint, yint)
+plt.show()
+
+
+# In[31]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y1 = np.array([1,2,3,4,5])
+y2 = np.array([5,6,7,8,9])
+y3 = np.array ([9,10,11,12,13])
+
+plt.plot(y1)
+plt.plot(y2)
+plt.plot(y3)
+
+plt.xlabel("X Label")
+plt.ylabel("Y Label")
+
+plt.show()
+
+
+# In[66]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#plot 1:
+xpoints = np.array([1,2,3,4])
+ypoints = np.array([1,2,3,4])
+
+plt.subplot(2,3,1)
+plt.plot(xpoints, ypoints, marker = 'D', linewidth = '4')
+
+plt.title("Simple Plot")
+
+#plot 2:
+y1 = np.array([12, 14, 17, 20])
+y2 = np.array([20, 10, 15, 10])
+y3 = np.array([0, 20, 17, 5])
+
+plt.subplot(2,3,2)
+plt.plot(y1, marker = 'o')
+plt.plot(y2, marker = '*')
+plt.plot(y3, marker = 'o')
+
+plt.title("Complex Plot")
+
+
+#plot 3:
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([0, 1, 4, 9])
+x2 = np.array([0, 2, 4, 6])
+y2 = np.array([0, 4, 8, 12])
+
+plt.subplot(2,3,3)
+plt.plot(x1, y1, x2, y2, linewidth = '10')
+plt.grid()
+
+plt.title("Intermediate Plot")
+plt.xlabel("Base Value")
+plt.ylabel("Mathematical Function")
+
+
+font1 = {'family': 'serif', 'color': 'red', 'size': 20}
+plt.suptitle("SUBPLOTS", fontdict = font1)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
